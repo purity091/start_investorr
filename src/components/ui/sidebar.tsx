f2476f3001c6693bd94400088d133a90b534a259
Mohamed Lahlah <your-email@example.com>
2026-07-26 20:07:49 +0300

    update

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -6,8 +6,8 @@ import { Slot } from "radix-ui"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/Button"
+import { Input } from "@/components/ui/Input"
 import { Separator } from "@/components/ui/separator"
 import {
   Sheet,
